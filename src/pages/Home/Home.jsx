9838db4525b73622eb6420003c37bea423aebcee
PabloTei <pteijeirolopez@gmail.com>
2023-03-19 17:22:44 +0100

    fixHome

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,14 +11,11 @@ const Home = () => {
   const { register, handleSubmit } = useForm();
   let navigate = useNavigate();
 
-
   const formSubmit = (formData) => {
-     API.post("/userclients", formData)
-     .then((res) => {
-      console.log(res.data)
-     })
+    API.post('/userclients', formData).then((res) => {
+      console.log(res.data);
+    });
   };
-  const formSubmit = (formData) => {};
 
   return (
     <main>
@@ -37,74 +34,68 @@ const Home = () => {
         <div className="container-address">
           <label htmlFor="address">Dirección</label>
 
-            <input 
-              type="text" 
-              id="address" 
-              name="street"
-              placeholder="Calle"
-              {...register('address.street')}
-            /> 
-            <input 
-              type="text" 
-              id="address" 
-              name="city"
-              placeholder="Ciudad"
-              {...register('address.city')}
-            />
-            <input 
-              type="text" 
-              id="address" 
-              name="region"
-              placeholder="C. Autónoma"
-              {...register('address.region')}
-            /> 
-            <input 
-            type="text" 
-            id="address" 
+          <input
+            type="text"
+            id="address"
+            name="street"
+            placeholder="Calle"
+            {...register('address.street')}
+          />
+          <input
+            type="text"
+            id="address"
+            name="city"
+            placeholder="Ciudad"
+            {...register('address.city')}
+          />
+          <input
+            type="text"
+            id="address"
+            name="region"
+            placeholder="C. Autónoma"
+            {...register('address.region')}
+          />
+          <input
+            type="text"
+            id="address"
             name="postalCode"
             placeholder="Código Postal"
             {...register('address.postalCode')}
-            /> 
+          />
         </div>
         <div className="container-phone">
           <label htmlFor="telephone">Teléfono</label>
-          <input 
-            type="text" 
-            id="telephone" 
+          <input
+            type="text"
+            id="telephone"
             name="telephone"
             placeholder="Teléfono"
             {...register('telephone')}
-            /> 
+          />
         </div>
         <div className="container-dni">
           <label htmlFor="dni">DNI</label>
-          <input 
-            type="text" 
-            id="dni" 
-            name="dni"
-            placeholder="DNI"
-            {...register('dni')}
-            /> 
+          <input type="text" id="dni" name="dni" placeholder="DNI" {...register('dni')} />
         </div>
         <div className="container-email">
           <label htmlFor="email">E-mail</label>
-          <input 
-            type="text" 
-            id="email" 
+          <input
+            type="text"
+            id="email"
             name="email"
             placeholder="E-mail"
             {...register('email')}
-            /> 
+          />
         </div>
         <div className="container-pwd">
           <label htmlFor="pwd">Contraseña</label>
-          <input 
-            type="password" 
-            id="pwd" 
+          <input
+            type="password"
+            id="pwd"
             name="pwd"
             placeholder="Contraseña"
             {...register('password')}
-            /> 
+          />
           <input
             type="text"
             id="adress"
@@ -144,7 +135,9 @@ const Home = () => {
             {...register('postalCode')}
           />
         </div>
-        <button type="submit" onClick={formSubmit}>Registrar</button>
+        <button type="submit" onClick={formSubmit}>
+          Registrar
+        </button>
       </form>
     </main>
   );
