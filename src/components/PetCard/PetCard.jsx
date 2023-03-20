284f00a8143dcfc8d9ac1abe221a90578a8c638f
virginia-zuror <virginiazuor@gmail.com>
2023-03-20 22:42:31 +0100

    consults

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -1,20 +1,15 @@
 import './PetCard.css';
-import Button from "../../UI/Button"
 
 const PetCard = ({ pet }) => {
   return (
-    <figure>
+    <>
       <img src={pet.photo} alt={pet.name} />
-      <div className='info-pet'>
-      <h2>Nombre: {pet.name}</h2>
-      <h3>Especie: {pet.specie}</h3>
-      <h3>Raza: {pet.breed}</h3>
+      <div className="info-pet">
+        <h2>Nombre: {pet.name}</h2>
+        <h3>Especie: {pet.specie}</h3>
+        <h3>Raza: {pet.breed}</h3>
       </div>
-      <div className='pet-buttons'>
-      <Button text="Consulta" padding="lg"/>
-      <Button text="Ver Perfil" padding="lg"/>
-      </div>
-    </figure>
+    </>
   );
 };
 
