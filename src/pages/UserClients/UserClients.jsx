84575aea9bde7ae2f2963549ef33140373282420
virginia-zuror <virginiazuor@gmail.com>
2023-03-20 18:01:51 +0100

    pets client

--- src/pages/UserClients/UserClients.jsx
+++ src/pages/UserClients/UserClients.jsx
@@ -1,26 +1,43 @@
 import './UserClients.css';
 
+import { useEffect, useState } from 'react';
+
+import AsideClient from '../../components/AsideClient/AsideClient';
 import PetCard from '../../components/PetCard/PetCard';
 import { API } from '../../services/API';
-import { useState } from 'react';
-import AsideClient from '../../components/AsideClient/AsideClient';
 
 const UserClients = () => {
-  const [user, setUser] = useState([]);
-  const [pets, setPets] = useState([]);
+  const [mascotas, setMascotas] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [allUsers, setAllUsers] = useState([]);
   const userLoged = JSON.parse(localStorage.getItem('user'));
+
   const getPets = () => {
     API.get('/userclients').then((res) => {
-      const allUsers = res.data;
-      allUsers.map((user) => user._id === userLoged._id && setUser(user));
-      setPets(user.pets);
+      setAllUsers(res.data);
+      allUsers.map((user) => user._id === userLoged._id && setMascotas(user.pets));
+
+      setLoaded(true);
     });
   };
-  getPets();
-  return <div>
-    <AsideClient/>
-    <main>Perfil cliente</main>
-    </div>;
+
+  useEffect(() => {
+    getPets();
+  }, [loaded]);
+
+  return (
+    <div>
+      <AsideClient />
+      <main>
+        <h3>Tus mascotas</h3>
+        {loaded ? (
+          mascotas?.map((mascota) => <PetCard pet={mascota} key={mascota._id} />)
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </main>
+    </div>
+  );
 };
 
 export default UserClients;
