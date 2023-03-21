87fc3f62f1fa03aa247baad6122bbc82608d0d27
virginia-zuror <virginiazuor@gmail.com>
2023-03-21 17:20:59 +0100

    consult+

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -1,26 +1,19 @@
 import './Pets.css';
 
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 import AsideStaff from '../../components/AsideStaff/AsideStaff';
 import PetCard from '../../components/PetCard/PetCard';
 import { API } from '../../services/API';
 import Button from '../../UI/Button';
-import Modal from '../../UI/Modal';
 
 const Pets = () => {
-  const { handleSubmit, register } = useForm();
+  const navigate = useNavigate();
+
   const [pets, setPets] = useState([]);
   const [filter, setFilter] = useState([]);
   const [loaded, setLoaded] = useState(false);
-
-  const [showModalConsult, setShowModalConsult] = useState(false);
-  const [petChosen, setPetChosen] = useState(null);
-  const typeUser = JSON.parse(localStorage.getItem('user'));
-  const clickConsult = (value) => {
-    setPetChosen(pets[value]);
-  };
 
   const getPets = () => {
     API.get('/pets').then((res) => {
@@ -30,17 +23,6 @@ const Pets = () => {
     });
   };
 
-  const formSubmit = (formData) => {
-    try {
-      API.post('/consults', formData).then((res) => {
-        console.log(res.data);
-        setShowModalConsult(false);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const filterFunction = (value) => {
     const petFilter = pets.filter((pet) => pet.name.toLowerCase().includes(value));
     setFilter(petFilter);
@@ -48,8 +30,7 @@ const Pets = () => {
 
   useEffect(() => {
     getPets();
-    console.log(petChosen);
-  }, [petChosen]);
+  }, []);
 
   return (
     <div>
@@ -73,78 +54,12 @@ const Pets = () => {
                     text="Nueva Consulta"
                     padding="lg"
                     action={() => {
-                      setShowModalConsult(true);
-                      clickConsult(index);
+                      localStorage.setItem('pet', JSON.stringify(pet));
+                      navigate('/consult');
                     }}
                   />
                   <Button text="Ver Perfil" padding="lg" />
-                  <Button text="Hitorial" padding="lg" />
                 </div>
-                {showModalConsult && (
-                  <Modal
-                    text="Registrar consulta"
-                    action={formSubmit}
-                    content={
-                      <div className="modal_consult">
-                        <h2>Nueva consulta</h2>
-                        <form onSubmit={handleSubmit(formSubmit)} id="formulario">
-                          <div className="container">
-                            <input
-                              type="text"
-                              id="date"
-                              name="date"
-                              required="on"
-                              {...register('date')}
-                            />
-                            <label htmlFor="date">Fecha</label>
-                          </div>
-                          <div className="container">
-                            <input
-                              type="text"
-                              id="vet"
-                              name="vet"
-                              value={typeUser.name}
-                              required="on"
-                              {...register('vet')}
-                            />
-                          </div>
-                          <div className="container exploracion">
-                            <input
-                              type="text"
-                              id="exp"
-                              name="exp"
-                              required="on"
-                              {...register('exp')}
-                            />
-                            <label htmlFor="exp">Exploración</label>
-                          </div>
-                          <div className="container">
-                            <input
-                              type="text"
-                              id="diagnose"
-                              name="diagnose"
-                              required="on"
-                              defaultValue=""
-                              {...register('diagnose')}
-                            />
-                            <label htmlFor="diagnose">Diagnóstico</label>
-                          </div>
-                          <div className="container exploracion">
-                            <input
-                              type="text"
-                              id="treatment"
-                              name="treatment"
-                              required="on"
-                              defaultValue=""
-                              {...register('treatment')}
-                            />
-                            <label htmlFor="treatment">Tratamiento</label>
-                          </div>
-                        </form>
-                      </div>
-                    }
-                  />
-                )}
               </figure>
             ))
           ) : (
