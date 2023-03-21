601896013edb544caa1b450964e618bab45e0a51
virginia-zuror <virginiazuor@gmail.com>
2023-03-21 22:28:51 +0100

    mas de staff_consult_appoints

--- src/pages/Staff/Staff.jsx
+++ src/pages/Staff/Staff.jsx
@@ -1,13 +1,40 @@
 import './Staff.css';
 
+import { useEffect, useState } from 'react';
+
 import AsideStaff from '../../components/AsideStaff/AsideStaff';
+import { API } from '../../services/API.js';
 
 const Staff = () => {
+  const [newAppointments, setNewAppointments] = useState([]);
+  const [pendingAp, setPendingAp] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const checkAppointments = () => {
+    API.get('/appointments').then((res) => {
+      setNewAppointments(res.data);
+      const appointFilter = newAppointments.filter(
+        (appointment) => appointment.checked === false,
+      );
+      setPendingAp(appointFilter);
+      setLoaded(true);
+    });
+  };
+
+  useEffect(() => {
+    checkAppointments();
+
+    console.log(pendingAp);
+  }, [loaded]);
+
   return (
-    <div>
+    <main className="editMain patients">
       <AsideStaff />
-      <main>Staff</main>
-    </div>
+      <section className="staff_overview">
+        {pendingAp.map((ap) => (
+          <h3 key={ap._id}>{ap.reason}</h3>
+        ))}
+      </section>
+    </main>
   );
 };
 
