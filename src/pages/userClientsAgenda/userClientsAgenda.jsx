0d2af96e9f5a491ac9e831d166853cce0d1bd90b
virginia-zuror <virginiazuor@gmail.com>
2023-03-22 22:52:02 +0100

    POR FIN CITAS DE CLIENTE SPM

--- src/pages/userClientsAgenda/userClientsAgenda.jsx
+++ src/pages/userClientsAgenda/userClientsAgenda.jsx
@@ -6,36 +6,60 @@ import AsideClient from '../../components/AsideClient/AsideClient';
 import { API } from '../../services/API';
 
 const UserClientsAgenda = () => {
-  const [clients, setClients] = useState([]);
+  const [petsFound, setPetsFound] = useState([]);
   const userInLocal = JSON.parse(localStorage.getItem('user'));
-  const [clientLogged, setClientLogged] = useState();
-  const [userLoaded, setUserLoaded] = useState(false);
+  const allPets = userInLocal.pets;
+  const [loaded, setLoaded] = useState(false);
+  const [petByUser, setPetByUser] = useState();
 
-  const getClients = () => {
-    API.get('/userClients').then((res) => {
-      setClients(res.data);
-      setUserLoaded(true);
-    });
-  };
-
-  const mapClients = () => {
-    clients.map((client) => 
-    {if(client._id == userInLocal._id){
-      setClientLogged(client);
-    }});
-    console.log(clientLogged);
+  let pets = [];
+  const getAllPets = () => {
+    API.get('/pets')
+      .then((res) => {
+        setPetsFound(res.data);
+        console.log(res.data);
+        petsFound.map((petFound) => {
+          if (allPets.includes(petFound._id)) {
+            pets.push(petFound);
+          }
+        });
+        setPetByUser(pets);
+        setLoaded(true);
+      })
+      .catch(console.log('error'));
   };
 
   useEffect(() => {
-    getClients();
-    mapClients();
-  }, []);
+    getAllPets();
+
+    console.log(petByUser);
+  }, [loaded === true]);
 
   return (
     <div className="content">
       <AsideClient />
       <main>
-        <p>{clientLogged?.name}</p>
+        {loaded === true ? (
+          petByUser.map((pet) =>
+            pet.appoint.map((ap) =>
+              ap.checked === true ? (
+                <figure key={ap._id}>
+                  <h4>{pet.name}</h4>
+                  <h5>{ap.date.toString().split('').slice(0, 10)}</h5>
+                  <h5>{ap.date.toString().split('').slice(11, 16)}</h5>
+                  <h4>Motivo:</h4>
+                  <p>{ap.reason}</p>
+                  <h4>Comentarios:</h4>
+                  <p>{ap.comments}</p>
+                </figure>
+              ) : (
+                <h4 key={pet._id}>{pet.name}: No tiene citas</h4>
+              ),
+            ),
+          )
+        ) : (
+          <h2>Loading...</h2>
+        )}
       </main>
     </div>
   );
