04cf2d2c8a094b5fcd153fb569b9488c8b8ee704
Laura VL <laura_villarreal.l@hotmail.com>
2023-03-23 19:15:26 +0100

    fixing dropdown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,82 +20,48 @@ const Header = () => {
   };
 
   return (
-    <div className="header">
-      <abbr title="Home">
-        <NavLink to="/">
-          <img
-            className="logo"
-            src="https://res.cloudinary.com/depifliz3/image/upload/v1679478181/Neovet_blanco_Mesa_de_trabajo_1_uii63w.png"
-            alt="logo-neovet"
-          />
-        </NavLink>
-      </abbr>
-      {user && typeUser.rol === 'userClient' && (
-        <div className="avatar-staff">
-          <h2>{user.name}</h2>
-        </div>
-      )}
-      {user && typeUser.rol === 'userAdmin' && (
-        <div className="avatar-staff">
-          <h2>{user.name}</h2>
-          <img src={user.avatar} alt={user.avatar} />
-        </div>
-      )}
-      <ul className="links">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-
-        {localStorage.getItem('user') && typeUser.rol === 'userAdmin' && (
+    <nav>
+        <img src="https://res.cloudinary.com/depifliz3/image/upload/v1679478181/Neovet_blanco_Mesa_de_trabajo_1_uii63w.png" alt="logo Neovet" />
+        <ul>
           <li>
-            <NavLink to="/staff">Staff</NavLink>
+            <NavLink to="/">Home</NavLink>
           </li>
-        )}
-        {localStorage.getItem('user') && typeUser.rol === 'userClient' && (
-          <>
+          {localStorage.getItem('user') && typeUser.rol === 'userAdmin' && (
+            <li>
+              <NavLink to="/staff">Staff</NavLink>
+            </li>
+          )}
+          {localStorage.getItem('user') && typeUser.rol === 'userClient' && (
             <li>
               <NavLink to="/userclients">Cliente</NavLink>
             </li>
-          </>
-        )}
-        {localStorage.getItem('token') ? (
-          <li>
-            <Button padding="lg" text="Logout" variant="contained" action={logout} />
-          </li>
-        ) : (
-          <>
-            <li>
-              <a href="#">Login</a>
-              <ol className="submenu">
-                <li className="menu-item">
-                  <NavLink to="/loginstaff">Login Staff</NavLink>
-                </li>
-                <li className="menu-item">
-                  <NavLink to="/loginclientes">Login Cliente</NavLink>
-                </li>
-              </ol>
-              {/* <select
-                defaultValue="default "
-                name="loginSelect"
-                onChange={(e) => {
-                  selection = e.target.value;
-                  loginSelected();
-                }}
-              >
-                <option value="default">Loguéate</option>
-                <option value="loginstaff">Login Staff</option>
-                <option value="loginclients">Login Clientes</option>
-              </select> */}
+          )}
+          {localStorage.getItem('token') ? (
+            <li className="logout-li">
+              <Button padding="lg" text="Logout" variant="contained" action={logout} />
             </li>
-            <li>
+          ) : (
+            <>
+              <li>
+                <NavLink to="#">Login</NavLink>
+                <ul>
+                  <li className="dropdown-li">
+                    <NavLink to="/loginstaff">Login Staff</NavLink>
+                  </li>
+                  <li className="dropdown-li">
+                    <NavLink to="/loginclientes">Login Clientes</NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li>
               <a href="/#formulario">
                 <Button text="Regístrate" padding="lg" variant="contained" />
               </a>
             </li>
-          </>
-        )}
-      </ul>
-    </div>
+            </>
+          )}
+        </ul>
+    </nav>
   );
 };
 
