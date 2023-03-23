23c46d67f74fea73b109fbde2891dbdcdfaafcaa
virginia-zuror <virginiazuor@gmail.com>
2023-03-23 15:06:22 +0100

    edit_client & staff_maquet

--- src/pages/EditClientProfile/EditClientProfile.jsx
+++ src/pages/EditClientProfile/EditClientProfile.jsx
@@ -13,6 +13,7 @@ const EditClientProfile = () => {
   const userLoged = JSON.parse(localStorage.getItem('user'));
   const [clkSave, setClkSave] = useState(false);
   const [submited, setSubmited] = useState(false);
+  const [result, setResult] = useState([]);
 
   const checking = () => {
     setClkSave(!clkSave);
@@ -21,6 +22,7 @@ const EditClientProfile = () => {
   const formSubmit = (formData) => {
     API.patch(`/userclients/${userLoged._id}`, formData).then((res) => {
       console.log(res.data);
+      setResult(res.data);
       setSubmited(true);
     });
   };
@@ -32,11 +34,25 @@ const EditClientProfile = () => {
         <h2>Edita tu perfil</h2>
         <form onSubmit={handleSubmit(formSubmit)} id="formulario_edit">
           <div className="container">
-            <input type="text" id="name" required {...register('name')} />
+            <input
+              type="text"
+              id="name"
+              defaultValue={userLoged.name}
+              required
+              autoFocus
+              {...register('name')}
+            />
             <label htmlFor="name">Nombre</label>
           </div>
           <div className="container">
-            <input type="text" id="telephone" required {...register('telephone')} />
+            <input
+              type="text"
+              id="telephone"
+              defaultValue={userLoged.telephone}
+              required
+              autoFocus
+              {...register('telephone')}
+            />
             <label htmlFor="telefono">Teléfono</label>
           </div>
           <div className="container">
@@ -44,7 +60,9 @@ const EditClientProfile = () => {
               type="text"
               id="street"
               name="street"
+              defaultValue={userLoged.address.street}
               required
+              autoFocus
               {...register('address.street')}
             />
             <label htmlFor="street">Calle</label>
@@ -54,7 +72,9 @@ const EditClientProfile = () => {
               type="text"
               id="city"
               name="city"
+              defaultValue={userLoged.address.city}
               required
+              autoFocus
               {...register('address.city')}
             />
             <label htmlFor="city">Ciudad</label>
@@ -64,7 +84,9 @@ const EditClientProfile = () => {
               type="text"
               id="region"
               name="region"
+              defaultValue={userLoged.address.region}
               required
+              autoFocus
               {...register('address.region')}
             />
             <label htmlFor="region">Provincia</label>
@@ -74,7 +96,9 @@ const EditClientProfile = () => {
               type="text"
               id="postalCode"
               name="postalCode"
+              defaultValue={userLoged.address.postalCode}
               required
+              autoFocus
               {...register('address.postalCode')}
             />
             <label htmlFor="postalCode">Código Postal</label>
@@ -91,7 +115,7 @@ const EditClientProfile = () => {
         </form>
         {submited && clkSave && (
           <Modal
-            content="Cambios realizados"
+            content="Cambios realizados, podrás visualizarlos volviendo a loguearte"
             action={() => checking()}
             text="X"
             className="modal_cambios"
@@ -99,6 +123,23 @@ const EditClientProfile = () => {
           />
         )}
       </section>
+
+      {submited === true ? (
+        <section className="resultsection">
+          <h3>Tus Cambios:</h3>
+          <p>{result.name}</p>
+          <p>{result.telephone}</p>
+          <h5>Dirección:</h5>
+          <p>{result.address.street}</p>
+          <p>{result.address.city}</p>
+          <div>
+            <p>({result.address.postalCode}) - </p>
+            <p>{result.address.region}</p>
+          </div>
+        </section>
+      ) : (
+        ''
+      )}
     </main>
   );
 };
