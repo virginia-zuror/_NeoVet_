5cf3d48023aa596ddb3025f07dc36e516a95a538
sergiohut <sergiosantiagocarmona@gmail.com>
2023-03-23 15:06:03 +0100

    appointment done

--- src/pages/UserClientsAppoint/UserClientsAppoint.jsx
+++ src/pages/UserClientsAppoint/UserClientsAppoint.jsx
@@ -6,69 +6,64 @@ import { useForm } from 'react-hook-form';
 import AsideClient from '../../components/AsideClient/AsideClient';
 import { API } from '../../services/API';
 import Button from '../../UI/Button';
-import Modal from '../../UI/Modal';
+
 
 const UserClientsAppoint = () => {
   const { handleSubmit, register } = useForm();
   const [submited, setSubmited] = useState(false);
-  const [staff, setStaff] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [admins, setAdmins] = useState([]);
-  const [client, setClient] = useState([]);
   const [clinic, setClinic] = useState([]);
 
   const petInAppoint = JSON.parse(localStorage.getItem('pet'));
   const userLocal = JSON.parse(localStorage.getItem('user'));
-  let arrayClinic = [];
-
-
+  let arrayMember = [];
 
   const getAdmins = () => {
     API.get('/admins').then((res) => {
       setAdmins(res.data);
-    });
+    }).then(()=>{mapAdmins();})
   };
   
   const mapAdmins = () => {
-    console.log("iniciando función")
     admins.map((item)=>{
       item.clients.map((cl)=>{
-        cl._id === userLocal._id && arrayClinic.push(item)
+        if (cl._id.includes(userLocal._id)) {
+          setClinic(...clinic, item)
+        } 
       })
     }) 
-    console.log(clinic)
-    setClinic(arrayClinic)
     setLoaded(true);
   };
-
 
   const formSubmit = (formData) => {
     const data = {
       date: formData.date,
-      vet: formData.vet,
+      staff: formData.staff,
       reason: formData.reason,
       comments: formData.comments,
       pet: petInAppoint._id,
     };
 
     API.post('/appointments', data).then((res) => {
-      console.log(res.data);
       setSubmited(true);
     });
   };
 
   useEffect(() => {
     getAdmins();
-    mapAdmins();
   }, [loaded]);
 
   return (
     <main className="appointMain">
       <AsideClient />
-      <section>
-        <form onSubmit={handleSubmit(formSubmit)}>
+      <section className='appointSection'>
+        <form onSubmit={handleSubmit(formSubmit)}
+        id="create_appoint">
           <h2>Solicitud de cita</h2>
+          <div className="firstRow">
           <div className="container">
+            <h4>Fecha</h4>
             <input
               type="date"
               id="date"
@@ -76,14 +71,15 @@ const UserClientsAppoint = () => {
               required="on"
               {...register('date')}
             />
-            <label htmlFor="date">Fecha</label>
           </div>
+          </div>
+          <div className="secondRow">
           <div className="container container-staff">
-            <select name="staff" id="staff" defaultValue="nothing">
-              <option value="nothing"></option>
-              {loaded ? (
+            <select name="staff" id="staff" defaultValue="nothing" {...register("staff")}>
+            <option value="nothing"></option>
+              {clinic.staff? (
                 clinic.staff.map((staffmember) => (
-                  <option key={staffmember._id} value={staffmember.name}>
+                  <option key={staffmember._id} value={staffmember._id}>
                     {staffmember.name}
                   </option>
                 ))
@@ -94,31 +90,30 @@ const UserClientsAppoint = () => {
             <label htmlFor="staff">Veterinario/auxiliar</label>
           </div>
           <div className="container container-admins">
-            <select name="staff" id="admins" defaultValue="none">
-              <option value={null}></option>
+            <select name="reason" id="reason" defaultValue="nothing" {...register("reason")}>
+              <option value="nothing"></option>
               <option value="vacuna">Vacuna</option>
               <option value="revisión">Revisión</option>
               <option value="consulta">Consulta</option>
               <option value="gestión">Gestión</option>
             </select>
-            <label htmlFor="staff">Motivo</label>
+            <label htmlFor="reason">Motivo</label>
+          </div>
           </div>
           <div className="container">
-            <input
+          <h3>Comentarios adicionales</h3>
+            <textarea className='comments'
               type="text"
               id="comments"
               name="comments"
               required="on"
               {...register('comments')}
-            />
-            <label htmlFor="comments">Comentarios adicionales</label>
+            ></textarea>
           </div>
           <Button
             padding="xl"
             type="button"
-            action={() => {
-              checking();
-            }}
+            action={formSubmit}
             text="Solicitar"
           />
         </form>
