9c62bf72e51bdad07284eb3026f58649930f4bc0
Laura VL <laura_villarreal.l@hotmail.com>
2023-03-28 09:41:51 +0200

    fixing aside

--- src/components/AsideClient/AsideClient.jsx
+++ src/components/AsideClient/AsideClient.jsx
@@ -10,52 +10,52 @@ const AsideClient = () => {
         <ul className="asideLinks">
           <li>
             <NavLink to="/userclients">
-              Vista General{' '}
               <img
                 className="icon"
                 src="https://res.cloudinary.com/ddu2qdsdp/image/upload/v1679307394/Neovet/person-circle-outline_yyfflv.svg"
                 alt="icono agenda"
               />
+              Vista General{' '}
             </NavLink>
           </li>
           <li>
             <NavLink to="/userclients_agenda">
-              Agenda{' '}
               <img
                 className="icon"
                 src="https://res.cloudinary.com/ddu2qdsdp/image/upload/v1679305138/Neovet/calendar-outline_vzwcnd.svg"
                 alt="icono agenda"
               />
+              Agenda{' '}
             </NavLink>
           </li>
           <li>
             <NavLink to="/userclients_staff">
-              Profesionales
               <img
                 className="icon"
                 src="https://res.cloudinary.com/ddu2qdsdp/image/upload/v1679320948/Neovet/stethoscope_FILL0_wght400_GRAD0_opsz48_n549qq.svg"
                 alt="icono pacientes"
               />
+              Profesionales
             </NavLink>
           </li>
           <li>
             <NavLink to="/userclients_createpet">
-              Añadir Mascota
               <img
                 className="icon"
                 src="https://res.cloudinary.com/depifliz3/image/upload/v1679342582/samples/animals/huella_hbss1b.png"
                 alt="icono huella"
               />
+              Añadir Mascota
             </NavLink>
           </li>
           <li>
             <NavLink to="/editprofile">
-              Editar Perfil{' '}
               <img
                 className="icon"
                 src="https://res.cloudinary.com/ddu2qdsdp/image/upload/v1679404997/Neovet/edit_FILL0_wght400_GRAD0_opsz48_iuflhs.svg"
                 alt="icono editar perfil"
               />
+              Editar Perfil{' '}
             </NavLink>
           </li>
         </ul>
